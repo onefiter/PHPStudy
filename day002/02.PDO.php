d3546cca2b8be6884bcd9cb24f8da39c831bcec4
dw <onefiter@gmail.com>
2016-12-05 09:26:20 +0800

    PDO

---
+++ day002/02.PDO.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Represents a connection between PHP and a database server.
+ */
+/**
+ * PHP Manual Function Reference Database Extensions Abstraction Layers PDO*
+ */
+
+$dsn = "mysql:host=localhost;port=3306;dbname=php";
+$opt = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8');
+$pdo = new PDO($dsn,'root','root',$opt);
+var_dump($pdo);
