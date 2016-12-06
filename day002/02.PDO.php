1861a05eee103dfa31735a43c5c737da2efe2548
dw <onefiter@gmail.com>
2016-12-07 01:10:50 +0800

    no

--- day002/02.PDO.php
+++ day002/02.PDO.php
@@ -9,4 +9,4 @@
 $dsn = "mysql:host=localhost;port=3306;dbname=php";
 $opt = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8');
 $pdo = new PDO($dsn,'root','root',$opt);
-var_dump($pdo);
+var_dump($pdo); 
