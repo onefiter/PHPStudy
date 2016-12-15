3b5ebf89eacef9d203f1dbedca5ff43815fe8518
dw <onefiter@gmail.com>
2016-12-15 23:26:38 +0800

    description

--- day005/01.directory_handle.php
+++ day005/01.directory_handle.php
@@ -3,7 +3,13 @@
  * Directory Handle
  */
 $path = "./path/where/nice";
-
+/**
+ * mkdir
+ * bool mkdir ( string $pathname [, int $mode = 0777 [, bool $recursive = false [, resource $context ]]] )
+ * @param $mode  读写
+ * @param $recursive 是否递归创建
+* @param $recursive 是否递归创建
+ */
 if (!is_dir($path))
 {
   $re = mkdir($path,0755,true);
