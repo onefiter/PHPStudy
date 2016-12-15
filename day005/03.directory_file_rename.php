ca1b1983109a1b467eacafcf65ee54b143f591b0
dw <onefiter@gmail.com>
2016-12-16 00:32:59 +0800

    add reference

--- day005/03.directory_file_rename.php
+++ day005/03.directory_file_rename.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Refer to PHP Manual -> Function Reference -> File System Related Extensions -> Filesystem -> Filesystem Functions
+ * @var string
+ */
 $oldpath = './newpath/there';
 $new_path = './newpath/where';
 
