ef9cb0046b549cb29660ff43064de5b496001a93
dw <onefiter@gmail.com>
2016-12-16 23:26:28 +0800

    read  present directory

--- day005/04.directory_handle_recursive.php
+++ day005/04.directory_handle_recursive.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * 读取当前目录的内容
+ * @param  string $path 路径
+ */
+function readDirs($path)
+{
+  $handle = opendir($path);
+  while (false !== ($filename = readdir($path)))
+  {
+    if ($filename == '.' || $filename == '..')
+    continue;
+    echo $filename, "<br/>";
+  }
+  closedir($handle);
+}
